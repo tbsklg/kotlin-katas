1d52bd355799da2eb3fbe5baf042cae0b87523d3
Tobias Klug <tbs.klg@gmail.com>
2022-11-11 10:10:10 +0100

    Solve 566fc12495810954b1000030: Count the Digit

--- src/main/kotlin/countdig/NbDig.kt
+++ src/main/kotlin/countdig/NbDig.kt
@@ -1,2 +1,16 @@
 package countdig
 
+import kotlin.math.pow
+
+fun nbDig(n: Int, d: Int): Int {
+    return generateSequence(0) { it + 1 }
+        .take(n + 1)
+        .map { it.toDouble() }
+        .map { it.pow(2) }
+        .map { it.toInt() }
+        .joinToString("")
+        .filter { it == '0' + d }
+        .length
+}
+
+//(0..n).map{ (it * it) }.sumBy { it.toString().count{ it == d.toString()[0] } }
--- src/test/kotlin/countdig/NbDigKtTest.kt
+++ src/test/kotlin/countdig/NbDigKtTest.kt
@@ -1,5 +1,18 @@
 package countdig
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class NbDigKtTest
+internal class NbDigKtTest {
+    private fun testing(actual: Int, expected: Int) {
+        assertEquals(expected.toLong(), actual.toLong())
+    }
+
+    @Test
+    fun test() {
+        println("Fixed Tests nbDig")
+        testing(nbDig(5750, 0), 4700)
+        testing(nbDig(11011, 2), 9481)
+
+    }
+}
